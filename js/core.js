f5356dd29b31880f8dc7e19ee4cd6d0d43e0a4ea
Sam Sartor <me@drocclusion.net>
2016-11-12 12:05:18 -0700

    Background Color

--- js/core.js
+++ js/core.js
@@ -1,10 +1,9 @@
 var stage;
-var canvas = document.getElementById('canvas'); 
 var transform = new Transform(0, 100, 0, 100, .5, 1, 1);
 
 function resize_canvas() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	stage.canvas.width = window.innerWidth;
+	stage.canvas.height = window.innerHeight;
 	transform.update(canvas.width, canvas.height);
 	transform.applyToContainer(stage);
 	stage.update();
@@ -17,10 +16,14 @@ function tick() {
 
 function canvas_init() {
 	stage = new createjs.Stage("canvas");
+
+	createjs.Ticker.setFPS(60);
 	createjs.Ticker.addEventListener("tick", tick);
 
+	stage.canvas.style.backgroundColor = "#ff0800";
+
 	var circle = new createjs.Shape();
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+	circle.graphics.beginFill("#FFFAFA").drawCircle(0, 0, 50);
 	circle.x = 50;
 	circle.y = 50;
 	stage.addChild(circle);
