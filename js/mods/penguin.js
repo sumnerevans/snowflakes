92e5ce758f67eaf264b249e9176381054267de0a
Jack Rosenthal <jack@rosenth.al>
2016-11-12 15:44:11 -0700

    wobble wobble..

--- js/mods/penguin.js
+++ js/mods/penguin.js
@@ -1,6 +1,9 @@
 var Penguin = function () {
     var image_front = new createjs.Bitmap('img/tux.png');
     image_front.tickEnabled = false;
+    image_front.regX = 277;
+    image_front.regY = 600;
+    var t = 0;
 
     var tallness;
 
@@ -13,13 +16,21 @@ var Penguin = function () {
     };
 
     this.resize = function () {
-        var scale = 20 / image_front.image.width;
+        var scale = 13 / image_front.image.width;
         image_front.x = transform.provide.minx + transform.provide.sizex * .5;
         image_front.scaleX = scale;
         image_front.scaleY = scale;
 
         tallness = scale * image_front.image.height;
-        image_front.y = transform.provide.maxy - stage.floor_height * .2 - tallness;
+        image_front.y = transform.provide.maxy
+                        - stage.floor_height * .2
+                        - tallness
+                        + image_front.regY * scale;
+    };
+
+    this.tick = function () {
+        image_front.rotation = 6*Math.sin(0.4*t);
+        t += 1;
     };
 };
 
